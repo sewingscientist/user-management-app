7b6eafb8c47142683a5a7bb24374a4bf6fb7f898
sewingscientist <111903780+sewingscientist@users.noreply.github.com>
2025-01-05 14:10:06 +0000

    minor edits

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ if os.getenv('JAWSDB_MARIA_URL'):
     logging.info(f"Database: {app.config['MYSQL_DB']}")
 else:
     # Connect to local MySQL database server - Required
-    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', '127.0.0.1 ')
+    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', '127.0.0.1')
     app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
     app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
     app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
